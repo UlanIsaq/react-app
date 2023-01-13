c23a0a0de0beef3d59fbc4ade1c37ce86e3b87df
ulugbekiskanadzhiev <kaltatay@gmail.com>
2023-01-13 13:32:52 -0500

    fiz: klsdjfflsd

--- release.config.js
+++ release.config.js
@@ -1,9 +1,20 @@
 module.exports = {
     branches: "master",
     repositoryUrl: "https://github.com/UlanIsaq/react-app",
-    plugins: [
+    plugins: 
+[
         '@semantic-release/commit-analyzer', 
         '@semantic-release/release-notes-generator', 
         '@semantic-release/npm', 
-        '@semantic-release/github']
-}
+    [
+            '@semantic-release/github',
+        {
+            assets: 
+            [
+                { path: "build.zip", label: "Build"},
+                { path: "coverage.zip, label: "Coverage"}
+            ]
+        }
+    ]
+]
+};
